b690ea4b6caf5ae8931b824b8c2a48aa35a54a34
Saiparimala <90768506+SParii@users.noreply.github.com>
2023-07-09 21:13:17 +0530

    saved code

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cbdl72fk96a1.us-east-1.rds.amazonaws.com"
+customuser = "admin"
+custompass = "admin123"
+customdb = "employee"
+custombucket = "addemployee45"
+customregion = "us-east-1"
